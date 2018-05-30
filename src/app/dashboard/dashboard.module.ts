78bf073c5c7cd4a6dedf736278837d17793353b5
j.bultez <jordan.bultez@gmail.com>
2018-05-30 16:52:14 +0200

    add gridster to the app, edit dashboard component and app scss

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutesModule } from './dashboard.routes';
+import { GridsterModule } from 'angular-gridster2';
+import { DashboardGridsterConfigService } from './dashboard-gridster-config.service';
 
 @NgModule({
   imports: [
     CommonModule,
-    DashboardRoutesModule
+    DashboardRoutesModule,
+    GridsterModule
   ],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent],
+  providers: [DashboardGridsterConfigService]
 })
 export class DashboardModule { }
